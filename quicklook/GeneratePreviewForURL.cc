5d513b5d5561c4b13efb2f6cb32eab287b6afd3b
Brandon Fosdick <bfoz@bfoz.net>
2010-01-01 15:20:54 -0800

    Link intelhex library
    Converted GeneratePreviewForURL.c and GenerateThumbnailForURL.c to C++ and marked the handlers as extern "C"

--- quicklook/GeneratePreviewForURL.cc
+++ quicklook/GeneratePreviewForURL.cc
@@ -2,19 +2,21 @@
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
 
+#include <intelhex.h>
+
 /* -----------------------------------------------------------------------------
    Generate a preview for file
 
    This function's job is to create preview for designated file
    ----------------------------------------------------------------------------- */
 
-OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
+extern "C" OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     #warning To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
     return noErr;
 }
 
-void CancelPreviewGeneration(void* thisInterface, QLPreviewRequestRef preview)
+extern "C" void CancelPreviewGeneration(void* thisInterface, QLPreviewRequestRef preview)
 {
     // implement only if supported
 }
--- quicklook/GenerateThumbnailForURL.cc
+++ quicklook/GenerateThumbnailForURL.cc
@@ -2,19 +2,21 @@
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
 
+#include <intelhex.h>
+
 /* -----------------------------------------------------------------------------
     Generate a thumbnail for file
 
    This function's job is to create thumbnail for designated file as fast as possible
    ----------------------------------------------------------------------------- */
 
-OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
+extern "C" OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
     #warning To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
     return noErr;
 }
 
-void CancelThumbnailGeneration(void* thisInterface, QLThumbnailRequestRef thumbnail)
+extern "C" void CancelThumbnailGeneration(void* thisInterface, QLThumbnailRequestRef thumbnail)
 {
     // implement only if supported
 }
