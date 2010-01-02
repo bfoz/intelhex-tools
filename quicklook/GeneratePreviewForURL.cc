e6a4e66f20c33444ce43e029fcd2668be09c9517
Brandon Fosdick <bfoz@bfoz.net>
2010-01-01 17:52:52 -0800

    First working QuickLook generator
    Returns the given file as plain text

--- quicklook/GeneratePreviewForURL.cc
+++ quicklook/GeneratePreviewForURL.cc
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <sstream>
+
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreServices/CoreServices.h>
 #include <QuickLook/QuickLook.h>
@@ -12,7 +15,30 @@
 
 extern "C" OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-    #warning To complete your generator please implement the function GeneratePreviewForURL in GeneratePreviewForURL.c
+    // Translate the CFURL into a file path because libintelhex uses STL to open
+    //  the file, which can't handle URLs
+    char buffer[FILENAME_MAX];
+    CFStringRef path = CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
+    if( !CFStringGetCString(path, buffer, sizeof(buffer), kCFStringEncodingASCII) )
+    {
+	CFRelease(path);
+	return -1;
+    }
+
+    // Load the file and convert it into a string
+    intelhex::container hex(buffer);
+    std::ostringstream ss;
+    hex.write(ss);
+
+    // Convert the string to a CFData object
+    CFDataRef data = CFDataCreate(NULL, (const UInt8*)ss.str().c_str(), ss.str().length());
+
+    // Pass the CFData as text for display by the standard handler
+    QLPreviewRequestSetDataRepresentation(preview, data, kUTTypePlainText, NULL);
+
+    CFRelease(path);
+    CFRelease(data);
+
     return noErr;
 }
 
